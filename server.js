f83a010ead0706d5c093b75cf7c3061e3019fde2
ShardulJoshi05 <shardulj.nmims@gmail.com>
2021-06-13 18:10:56 +0530

    Checkpoint 6

--- server.js
+++ server.js
@@ -1,14 +1,58 @@
-const express  = require('express');
-const app = express();
+var express = require("express"),
+  http = require("http"),
+  fs = require("fs"),
+  path = require("path"),
+  cors = require("cors");  //TODO:
 
-app.get("/movies",(req, res, next)=>{
-    res.send("All Movies Data in JSON format from Mongo DB")
-})
-app.get("/genres",(req, res, next)=>{
-    res.send("All genres Data in JSON format from Mongo DB")
-})
-app.get("/artists",(req, res, next)=>{
-    res.send("All artists Data in JSON format from Mongo DB")
-})
 
-app.listen(9000);
+
+// Running on posrt 9000
+const PORT = 3000;
+
+
+var app = express();
+
+var corsOptions = {
+  origin: "http://localhost:"+PORT
+};
+
+app.use(cors(corsOptions));
+
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// app.get("/movie", findAllMovies);
+// app.get('',findOne);
+
+// app.get('/genres',function(req,res){
+//     res.send("All Genres Data in JSON format from Mongo DB")
+// })
+
+// app.get('/artists',function(req,res){
+//     res.send("All Artists Data in JSON format from Mongo DB")
+// })
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Upgrad Movie booking application development.",
+  });
+});
+
+require("./routes/movie.routes")(app);
+require("./routes/artist.routes")(app);
+require("./routes/genre.routes")(app);
+// require("./routes/user.routes")(app)
+
+app.listen(PORT, function () {
+  console.log("express has started on port ", PORT);
+});
